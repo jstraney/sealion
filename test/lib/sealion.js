80385eff93742dd0bc6995aa65d6e5c21784fade
Jeffrey (Jeff) Straney <straney.jeffrey@ocls.info>
2020-12-15 18:49:04 -0500

    revised sealions recursive require mechanism. updated with unit tests

--- test/lib/sealion.js
+++ test/lib/sealion.js
@@ -1,5 +1,6 @@
 const {
-  sealionRequireClis,
+  sealionRequireCoreClis,
+  sealionRequirePluginClis,
   sealionRequirePlugins,
   sealionInstallPlugin,
   sealionUninstallPlugin,
@@ -7,6 +8,21 @@ const {
 
 const logger = require('../../lib/logger')('Test libSealion');
 
+const assert = require('assert');
+
 module.exports = async () => {
+
+  const coreClis = sealionRequireCoreClis();
+
+  assert(coreClis.hasOwnProperty('init'));
+  assert(typeof coreClis.init === 'function');
+
+  const pluginClis = sealionRequirePluginClis();
+
+  assert(pluginClis.hasOwnProperty('helloSealion'));
+  assert(typeof pluginClis.helloSealion === 'function');
+  assert(await pluginClis.helloSealion() === 'hello sealion');
+
+  logger.info('passed all sealion lib tests');
 
 }
